241fc43930d113861f3e6eb8f47584b11b2ebb21
Axel Schneider <axel.schneider2003@gmail.com>
2026-05-09 20:45:04 +0200

    Refactor GameScreen rendering logic and improve road drawing calculations

--- src/screens/GameScreen.scala
+++ src/screens/GameScreen.scala
@@ -2,11 +2,13 @@ package ch.hevs.fastandmudry
 package screens
 
 import ch.hevs.gdx2d.lib.GdxGraphics
+import com.badlogic.gdx.Gdx
 import com.badlogic.gdx.graphics.Color
 
 class GameScreen extends AbstractScreen {
 
   var carRoadPosition = 0f;
+  var carDistance = 0f
 
   val CAR_WIDTH = 33;
   val CAR_HEIGHT = 100;
@@ -14,35 +16,34 @@ class GameScreen extends AbstractScreen {
 
   override def onInit(): Unit = { }
 
-  override def onGraphicRender(g: GdxGraphics): Unit = {g.clear()
+  override def onGraphicRender(g: GdxGraphics): Unit = {
     g.clear()
     g.setColor(Color.WHITE)
     g.drawStringCentered(g.getScreenHeight - 50, "GAME VIEW !")
 
+    carDistance += 100f * Gdx.graphics.getDeltaTime
+
+    val middlePoint = 0.5f
+
     for(y <- 0 to (g.getScreenHeight / 2)) {
-      for(x <- 0 to g.getScreenWidth) {
-        val perspective = y / (g.getScreenHeight / 2f)
-        val middlePoint = 0.5f
-        var roadWidth = 0.9f - perspective * 0.8f
-        val clipWidth = roadWidth * 0.15f
+      val perspective = 1f - y / (g.getScreenHeight / 2f)
+      var roadWidth = 0.1f + perspective * 0.8f
+      val clipWidth = roadWidth * 0.15f
 
-        roadWidth *= 0.5f
+      roadWidth *= 0.5f
 
-        val leftGrass = (middlePoint - roadWidth - clipWidth) * g.getScreenWidth
-        val leftClip = (middlePoint - roadWidth) * g.getScreenWidth
-        val rightGrass = (middlePoint + roadWidth + clipWidth) * g.getScreenWidth
-        val rightClip = (middlePoint + roadWidth) * g.getScreenWidth
+      val leftGrass = (middlePoint - roadWidth - clipWidth) * g.getScreenWidth
+      val leftClip = (middlePoint - roadWidth) * g.getScreenWidth
+      val rightGrass = (middlePoint + roadWidth + clipWidth) * g.getScreenWidth
+      val rightClip = (middlePoint + roadWidth) * g.getScreenWidth
 
-        if (x >= 0 && x < leftGrass || x > rightGrass) {
-          g.setPixel(x, y, Color.FOREST)
-        }
-        else if (x < leftClip || x > rightClip) {
-          g.setPixel(x, y, if(y % 100 < 50) Color.RED else Color.WHITE)
-        }
-        else if (x < rightClip) {
-          // let black
-        }
-      }
+      val waveColor = math.sin(20f * math.pow(1f - perspective, 3) + carDistance * 0.1f).toFloat
+      val grassColor = new Color(0.1f, 0.5f + (waveColor * 0.2f), 0.1f, 1.0f)
+
+      g.drawLine(0, y, leftGrass, y, grassColor)
+      g.drawLine(leftGrass, y, leftClip, y, if(waveColor < 0.2) Color.WHITE else Color.RED)
+      g.drawLine(rightClip, y, rightGrass, y, if(waveColor < 0.2) Color.WHITE else Color.RED)
+      g.drawLine(rightGrass, y, g.getScreenWidth, y, grassColor)
     }
 
     // draw car
