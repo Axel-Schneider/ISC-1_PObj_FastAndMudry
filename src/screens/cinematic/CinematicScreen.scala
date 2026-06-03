08f995346c9540b678bc64f73c314c9054bdfe5a
Helder <helder2006hr@gmail.com>
2026-06-03 19:36:00 +0200

    add background and moving car in background screen

--- src/screens/cinematic/CinematicScreen.scala
+++ src/screens/cinematic/CinematicScreen.scala
@@ -4,12 +4,16 @@ package screens.cinematic
 import core.state.{EndDayCinematicEnded, FinalCinematicEnded, GameState, GameStateMachine, StartDayCinematicEnded}
 import screens.AbstractScreen
 
+import utils.Constant.CINEMATIC
+import ch.hevs.gdx2d.components.bitmaps.BitmapImage
 import ch.hevs.gdx2d.lib.GdxGraphics
 import com.badlogic.gdx.Gdx
 
 class CinematicScreen extends AbstractScreen {
   private var timeElapsed: Float = 0f
   private val DURATION: Float = 5f
+  private val background: BitmapImage = new BitmapImage(CINEMATIC.BACKGROUND_IMAGE)
+  private val carImage: BitmapImage = new BitmapImage(CINEMATIC.CAR_IMAGE)
 
   override def onInit(): Unit = {
     timeElapsed = 0f
@@ -18,7 +22,8 @@ class CinematicScreen extends AbstractScreen {
   override def onGraphicRender(g: GdxGraphics): Unit = {
     g.clear()
     timeElapsed += Gdx.graphics.getDeltaTime
-    g.drawStringCentered(g.getScreenHeight / 2f, "Cinematic")
+    drawBackground(g)
+    drawCar(g, 0f, 0f, g.getScreenWidth.toFloat / 2, carImage.getImage.getHeight.toFloat)
 
     if (timeElapsed >= DURATION) {
       GameStateMachine.getGameState match {
@@ -28,5 +33,34 @@ class CinematicScreen extends AbstractScreen {
         case _ => // nothing
       }
     }
+  }
+
+  private def drawBackground(g: GdxGraphics): Unit = {
+    val screenW = g.getScreenWidth.toFloat
+    val screenH = g.getScreenHeight.toFloat
+    val imgW = background.getImage.getWidth.toFloat
+    val imgH = background.getImage.getHeight.toFloat
+
+    val scale = math.min(screenW / imgW, screenH / imgH)
+    val drawW = imgW * scale
+    val drawH = imgH * scale
+
+    g.drawTransformedPicture(screenW / 2f, screenH / 2f, 0f, drawW / 2f, drawH / 2f, background)
+  }
+
+  private def drawCar(g: GdxGraphics, startX: Float, startY: Float, endX: Float, endY: Float): Unit = {
+    val imgW = carImage.getImage.getWidth.toFloat
+    val imgH = carImage.getImage.getHeight.toFloat
+
+    val dirX = if(startX > endX) -1 else 1
+    val dirY = if(startY > endY) -1 else 1
+
+    var x = dirX * timeElapsed*400f
+    var y = dirY * timeElapsed*200f
+
+    x = if(x <= endX) x else endX
+    y = if(y <= endY) y else endY
+
+    g.drawTransformedPicture(startX + x, startY + y, 0f, imgW / 2f, imgH / 2f, carImage)
   }
 }
--- src/utils/Constant.scala
+++ src/utils/Constant.scala
@@ -72,6 +72,10 @@ object Constant {
     val BACKGROUND_IMAGE: String = "data/images/garage/bg.png"
     val CAR_IMAGE: String = "data/images/garage/car.png"
   }
+  object CINEMATIC {
+    val BACKGROUND_IMAGE: String = "data/images/cinematic/bg.png"
+    val CAR_IMAGE: String = "data/images/cinematic/car.png"
+  }
   object GAME {
     object CAR {
       object FACTOR {
