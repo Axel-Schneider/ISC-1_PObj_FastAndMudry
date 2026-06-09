138e16f279081c51f762bb90d2683d62fecd0569
Helder <helder2006hr@gmail.com>
2026-06-09 22:14:41 +0200

    add bus postal car skin

--- src/core/car/CarSkin.scala
+++ src/core/car/CarSkin.scala
@@ -43,6 +43,12 @@ object CarSkins {
       WheelPosition(0.35f, 0.18f),
       SpeedometerConfig(0.28f, 0.19f, 80f, Color.BLACK, Color.WHITE, Color.RED),
     ),
+    CarSkin("Jacquemet", "Car Postal",
+      prefix + "bus" + threeQuarterImagePathSuffix, prefix + "bus" + sideImagePathSuffix, prefix + "bus" + openHoodImagePathSuffix,
+      prefix + "bus" + interiorImagePathSuffix, prefix + "bus" + steeringWheelImagePathSuffix,
+      WheelPosition(0.19f, -0.1f),
+      SpeedometerConfig(0.1f, 0f, 80f, Color.BLACK, Color.WHITE, Color.RED),
+    ),
   )
 
   val default: CarSkin = all(0)
