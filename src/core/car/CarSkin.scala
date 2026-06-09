50feab98af2b95bdf96de8911c77ae8fe2a1a763
Helder <helder2006hr@gmail.com>
2026-06-09 22:38:02 +0200

    add toyota supra

--- src/core/car/CarSkin.scala
+++ src/core/car/CarSkin.scala
@@ -49,6 +49,12 @@ object CarSkins {
       WheelPosition(0.19f, -0.1f),
       SpeedometerConfig(0.1f, 0f, 80f, Color.BLACK, Color.WHITE, Color.RED),
     ),
+    CarSkin("Paul Walker", "Toyota Supra MK4",
+      prefix + "supra" + threeQuarterImagePathSuffix, prefix + "supra" + sideImagePathSuffix, prefix + "supra" + openHoodImagePathSuffix,
+      prefix + "supra" + interiorImagePathSuffix, prefix + "supra" + steeringWheelImagePathSuffix,
+      WheelPosition(0.22f, 0.02f),
+      SpeedometerConfig(0.187f, 0.127f, 80f, Color.BLACK, Color.ORANGE, Color.ORANGE),
+    ),
   )
 
   val default: CarSkin = all(0)
